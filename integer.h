01498070de4ac5285444dcb3560dafe72787f802
Vijesh <ghandarevb14.comp@coep.ac.in>
2015-10-25 12:13:54 +0530

    Create integer.h

---
+++ integer.h
@@ -0,0 +1,15 @@
+typedef struct node {
+	char *s;
+	struct node *next, *prev;
+}node;
+typedef struct Integer{
+	char sign;
+	node *head, *tail;
+}Integer; 
+Integer CreateIntegerFromString(char *str);
+Integer AddIntegers(Integer a, Integer b);
+Integer SubstractIntegers(Integer a, Integer b);
+Integer MultiplyIntegers(Integer a, Integer b); 
+Integer DivideIntegers(Integer a, Integer b);
+int CompareIntegers(Integer a, Integer b);
+void PrintInteger(Integer a);
