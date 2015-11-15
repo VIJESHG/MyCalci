2e314e18705370f6b255a522f3075c0d8b847eed
Vijesh <ghandarevb14.comp@coep.ac.in>
2015-11-15 14:23:15 +0530

    Update exprtn.c

--- exprtn.c
+++ exprtn.c
@@ -1,20 +1,37 @@
+/*****************************************************************************
+ * Copyright (C) Vijesh B.G. vijay.ghandare@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 #include <stdio.h>
 #include "stack.h"
 #include <string.h>
 #include <stdlib.h>
 stack p;
-cstack o;
-Integer infix(char *arr){
+cstack q;
+Integer infix(char *arr) {
 int precedance(char c);
 void operation(char c);
 	int k;
-	int i = 0,j = 0,m,r,state = 0; 
-	char c,d[2];
+	int i = 0, j = 0, m, r, state = 0; 
+	char c, d[2]; 
 	char str[128];
 	init(&p);
-	cinit(&o);
-	for(r = 0; r < 127; r++){
-		str[r] = '0';
+	cinit(&q);
+	for(r = 0; r < 127; r++) {  							/* init string to zero */
+		str[r] = '0';		
 	}
 	str[r] = '\0';	
 	while(arr[i] != '\0'){
@@ -23,62 +40,57 @@ void operation(char c);
 			case '5': case'6': case '7': case '8':
 			case '9': case'0':
 			
-				str[j] = arr[i];
+				str[j] = arr[i];       					/* store digits in an array */
 				state = 1;
 				i++;
 				j++;
-				k = j;
+				k = j;                					/* remember the last position to convert array into string */
 				break;
 
 			case'+':
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ',' ){
+				if(arr[i-1] != ')' && arr[i-1] != ' '){
 					if(state == 1) {
-					Integer I;
-					j = 0;
-					str[k] = '\0';
-					I = CreateIntegerFromString(str);
-				      //PrintInteger(I);
-					push(&p, I);
-					state = 0;
+						Integer I;
+						j = 0;
+						str[k] = '\0';           		/* convert digit array to string */
+						I = CreateIntegerFromString(str);
+						push(&p, I);
+						state = 0;
 					}
 					else {
-						for(r = 0; r < 127; r++){
-						str[r] = '0';
-					}	
+						for(r = 0; r < 127; r++){ 		/* init srting to zero */
+							str[r] = '0';
+						}	
 						str[r] = '\0';
 						Integer I;
 						I = CreateIntegerFromString(str);
-						//printf("a->");
-				      	//PrintInteger(I);
 						push(&p, I);
 					}
 					
 				}
-				if(cisempty(&o) != 0){
-					if(precedance(*ctop(&o)) <= 20)
-						cpush(&o, arr[i]);
-					else{
-						while(precedance(*ctop(&o)) > 20){
-							operation(*ctop(&o));
-							c = cpop(&o);
+				if(cisempty(&q) != 0){
+					if(precedance(*ctop(&q)) <= 20)    		/* top operator has less or equal prec */
+						cpush(&q, arr[i]);
+					else {
+						while(precedance(*ctop(&q)) > 20) {  /* solve untill we get smaller prec */
+							operation(*ctop(&q));
+							c = cpop(&q);                  /* pop solved operator */
 						}
-						cpush(&o, arr[i]);
+						cpush(&q, arr[i]);                   /* push curr oprtr */
 					}
 				}
 				else
-					cpush(&o, arr[i]);
+					cpush(&q, arr[i]);                        /* if curr oprtr is first push it */
 				i++;
 				break;
 				
 			case'-':
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ','){
+				if(arr[i-1] != ')' && arr[i-1] != ' '){
 					if(state == 1) {
 					Integer I;
 					j = 0;
 					str[k] = '\0';
 					I = CreateIntegerFromString(str);
-					//printf("in-->");
-				    //PrintInteger(I);
 					push(&p, I);
 					state = 0;
 					}
@@ -89,82 +101,78 @@ void operation(char c);
 						str[r] = '\0';
 						Integer I;
 						I = CreateIntegerFromString(str);
-						//printf("a->");
-				      	//PrintInteger(I);
 						push(&p, I);
 					}
 				}
-				if(cisempty(&o) != 0){
-					if(precedance(*ctop(&o)) == 10)
-						cpush(&o, arr[i]);
+				if(cisempty(&q) != 0){
+					if(precedance(*ctop(&q)) == 10)
+						cpush(&q, arr[i]);
 					else{
-						while(precedance(*ctop(&o)) > 10){	
-							operation(*ctop(&o));
-							c = cpop(&o);
+						while(precedance(*ctop(&q)) > 10){	
+							operation(*ctop(&q));
+							c = cpop(&q);
 						}
-						cpush(&o, arr[i]);
+						cpush(&q, arr[i]);
 					}
 				}
 				else
-					cpush(&o, arr[i]);
+					cpush(&q, arr[i]);
 				i++;	
 				break;
 			case'%':
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ',' ){
+				if(arr[i-1] != ')' && arr[i-1] != ' '){
 					j = 0;
 					Integer I;
 					str[k] = '\0';
 					I = CreateIntegerFromString(str);
-				      //PrintInteger(I);
 					push(&p, I);
 					state = 0;
 				}
-				if(cisempty(&o) != 0){
-					if(precedance(*ctop(&o)) <= 25)
-						cpush(&o, arr[i]);
+				if(cisempty(&q) != 0){
+					if(precedance(*ctop(&q)) <= 30)
+						cpush(&q, arr[i]);
 					else{
-						while(precedance(*ctop(&o)) > 25){
-							operation(*ctop(&o));
-							c = cpop(&o);
+						while(precedance(*ctop(&q)) > 30){
+							operation(*ctop(&q));
+							c = cpop(&q);
 						}
-						cpush(&o, arr[i]);
+						cpush(&q, arr[i]);
 					}
 				}
 				else
-					cpush(&o, arr[i]);
+					cpush(&q, arr[i]);
 				i++;
 				break;
 			
 		
 			case'*': 
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ','){
+				if(arr[i-1] != ')' && arr[i-1] != ' '){
 				      j = 0;
 					str[k] = '\0';
 					Integer I;
 					I = CreateIntegerFromString(str);
-					//printf("in*->");
 				     // PrintInteger(I);
 					push(&p, I);
 					state = 0;
 				}
-				if(cisempty(&o) != 0){
-					if(precedance(*ctop(&o)) <= 30)
-						cpush(&o, arr[i]);
+				if(cisempty(&q) != 0){
+					if(precedance(*ctop(&q)) <= 40)
+						cpush(&q, arr[i]);
 					else{
-						while(precedance(*ctop(&o)) > 30){	
-						operation(*ctop(&o));
-						c = cpop(&o);	
+						while(precedance(*ctop(&q)) > 40){	
+						operation(*ctop(&q));
+						c = cpop(&q);	
 						}
-						cpush(&o, arr[i]);
+						cpush(&q, arr[i]);
 					}
 				}
 				else
-					cpush(&o, arr[i]);
+					cpush(&q, arr[i]);
 				i++;	
 				break;
 				
 			case'/': 
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ','){
+				if(arr[i-1] != ')' && arr[i-1] != ' '){
 					j = 0;
 					str[k] = '\0';
 					Integer I;
@@ -173,58 +181,57 @@ void operation(char c);
 					push(&p, I);
 					state = 0;
 				}
-				if(cisempty(&o) != 0){
-					if(precedance(*ctop(&o)) <= 40)
-						cpush(&o,arr[i]);
+				if(cisempty(&q) != 0){
+					if(precedance(*ctop(&q)) <= 50)
+						cpush(&q,arr[i]);
 					else{
-						while(precedance(*ctop(&o)) > 40){	
-						operation(*ctop(&o));
-						c = cpop(&o);	
+						while(precedance(*ctop(&q)) > 50){	
+						operation(*ctop(&q));
+						c = cpop(&q);	
 						}
-						cpush(&o, arr[i]);
+						cpush(&q, arr[i]);
 					}
 				}
 				else
-					cpush(&o, arr[i]);
+					cpush(&q, arr[i]);
 				i++;	
 				break;
 			
 			case'(':
-				cpush(&o, arr[i]);
-				//printf("%c", arr[i]);
+				cpush(&q, arr[i]);              			/* opening brace */
 				i++;
 				break;
 				
 			case')':
-				if(arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ','){
+				if(arr[i-1] != ')' && arr[i-1] != ' ') {
 					j = 0;
 					str[k] = '\0';
 					Integer I;
 					I = CreateIntegerFromString(str);
-					//printf("push->");
 				      //PrintInteger(I);
 					push(&p, I);
 					state = 0;
 				}
-				while(*ctop(&o) != '('){
-			        	operation(*ctop(&o));
-					c = cpop(&o);	
+				while(*ctop(&q) != '(') {            		/* solve untill we get opening brace */
+			        	operation(*ctop(&q));	
+					c = cpop(&q);	
 				}
-				c = cpop(&o);
+				c = cpop(&q);                       		/* pop opening brace */
 				i++;	
 				break;
 		
-			case ' ':
+			case ' ':                                 		/* skip spaces */
 			 	i++;				
 			 	break;
-			default:i++;
-				printf("Error!\n");
+			default:
+				i++;
+				printf("Invalid Expression\n");
 				exit(1);
 				break;
 		}
 	}
-
-	if(arr[i] == '\0' &&arr[i-1] != ')' && arr[i-1] != ' ' && arr[i-1] != ',') {
+	
+	if(arr[i] == '\0' && arr[i-1] != ')' && arr[i-1] != ' ') {
 		str[k] = '\0';
 		Integer I;
 		I = CreateIntegerFromString(str);
@@ -232,9 +239,9 @@ void operation(char c);
 		push(&p, I);
 		state = 0;
 	}
-	while(cisempty(&o) != 0){
-				operation(*ctop(&o));
-				c = cpop(&o);
+	while(cisempty(&q) != 0) {             					/* solve remaining */
+				operation(*ctop(&q));
+				c = cpop(&q);
 	}
 	return top(&p);
 	
@@ -242,7 +249,6 @@ void operation(char c);
 
 void operation(char c){
 	Integer x,y,z;
-	//int a,b;
 	x = pop(&p);
 	y = pop(&p);
 	switch(c){	
@@ -252,15 +258,12 @@ void operation(char c){
 			break;
 		case'-':
 			z = SubstractIntegers(y, x);
-			//printf("ans->");
-			//PrintInteger(z);
 			push(&p, z);
 			break;
-		/*case '%':
-			a = x;
-			b = y;
-			push(&p, b%a);
-			break;*/	
+		case '%':
+			z = ModIntegers(y,x);
+			push(&p, z);
+			break;	
 		case'*':
 			z = MultiplyIntegers(x, y);
 			push(&p, z);
@@ -278,11 +281,11 @@ int precedance(char c){
 	else if( c == '+')
 		return 20;
 	else if( c == '%')
-		return 25;
-	else if( c == '*')
 		return 30;
-	else if(c == '/')
+	else if( c == '*')
 		return 40;
+	else if(c == '/')
+		return 50;
 	else if(c =='(')
 		return 0;
 }
