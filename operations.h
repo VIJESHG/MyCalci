bec9d696a6db5ae12e3ff8f9e49de730c64e6dd0
Vijesh <ghandarevb14.comp@coep.ac.in>
2015-11-15 14:43:04 +0530

    Update operations.h

--- operations.h
+++ operations.h
@@ -9,8 +9,11 @@ typedef struct Integer{
 Integer CreateIntegerFromString(char *str);
 Integer AddIntegers(Integer a, Integer b);
 Integer SubstractIntegers(Integer a, Integer b);
-Integer MultiplyIntegers(Integer a, Integer b); 
+Integer ModIntegers(Integer a, Integer b);
 Integer DivideIntegers(Integer a, Integer b);
+Integer MultiplyIntegers(Integer a, Integer b);
 int CompareIntegers(Integer a, Integer b);
+Integer AddDigitToInteger(Integer a, char digit);
+Integer inInt(Integer a,char digit);
+void DecToHex(Integer DeciNum);
 void PrintInteger(Integer a);
-/*operatons*/
